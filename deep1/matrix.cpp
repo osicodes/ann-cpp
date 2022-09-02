d9eb4c983af73fb1332caa7dda748d9e43d676e7
Osinachi Mbah <osinachi99@gmail.com>
2022-09-02 23:46:05 +0300

    mnew

--- deep1/matrix.cpp
+++ deep1/matrix.cpp
@@ -8,12 +8,12 @@ using namespace std;
 #define MAXCHAR 100
 
 Matrix* matrix_create(int row, int col) {
-	Matrix* matrix = (Matrix *) malloc(sizeof(Matrix));
+	Matrix* matrix = (Matrix*)malloc(sizeof(Matrix));
 	matrix->rows = row;
 	matrix->cols = col;
-	matrix->entries = (double**) malloc(row * sizeof(double*)); //new double* [row];
+	matrix->entries = (double**)malloc(row * sizeof(double*)); //new double* [row];
 	for (int i = 0; i < row; i++) {
-		matrix->entries[i] = (double*) malloc(col * sizeof(double));
+		matrix->entries[i] = (double*)malloc(col * sizeof(double));
 	}
 	return matrix;
 }
@@ -64,7 +64,7 @@ Matrix* matrix_copy(Matrix* m) {
 	return mat;
 }
 
-/*void matrix_save(Matrix* m, char* file_string) {
+void matrix_save(Matrix* m, const char* file_string) {
 	FILE* file = fopen(file_string, "w");
 	fprintf(file, "%d\n", m->rows);
 	fprintf(file, "%d\n", m->cols);
@@ -75,9 +75,9 @@ Matrix* matrix_copy(Matrix* m) {
 	}
 	printf("Successfully saved matrix to %s\n", file_string);
 	fclose(file);
-}*/
+}
 
-/*Matrix* matrix_load(char* file_string) {
+Matrix* matrix_load(const char* file_string) {
 	FILE* file = fopen(file_string, "r");
 	char entry[MAXCHAR];
 	fgets(entry, MAXCHAR, file);
@@ -94,7 +94,7 @@ Matrix* matrix_copy(Matrix* m) {
 	printf("Sucessfully loaded matrix from %s\n", file_string);
 	fclose(file);
 	return m;
-}*/
+}
 
 double uniform_distribution(double low, double high) {
 	double difference = high - low; // The difference between the two
@@ -102,6 +102,93 @@ double uniform_distribution(double low, double high) {
 	int scaled_difference = (int)(difference * scale);
 	return low + (1.0 * (rand() % scaled_difference) / scale);
 }
+
+
+void my_matrix_randomize(Matrix* m, int in, int cu, const char* activefunc) {
+	/*
+	in -->  Number of nodes/neurons in input or previous layer (or number of input neurons coming into this current layer)
+	cu -->  Number of nodes/neurons in current layer (not input or previous layer)
+	size_l = cu;
+	size_l_minus_1 = in;
+
+	LeCun  Initialization (Good for SELU activation functions )
+	----------------------
+	   Min: -1 / sqrt(in)
+	   Max: 1 / sqrt(in)
+	---------------------
+
+
+	Xavier normalized initialization  (Good for other activation functions{Linear, sigmoid, tanh, softmax, logistics},
+													 but not very good for RELU)
+	 sqrt(1 / ave)
+	 ave = (in + cu) / 2
+	----------------------
+	   Min: -sqrt(2 / (size_l_minus_1 + size_l))
+	   Max: sqrt(2 / (size_l_minus_1 + size_l))
+	---------------------
+
+
+	Xavier uniform initialization
+	----------------------
+	   Min: -(sqrt(6.0) / sqrt(size_l_minus_1 + size_l))
+	   Max: (sqrt(6.0) / sqrt(size_l_minus_1 + msize_l))
+	---------------------
+
+
+	He Initialization (Good for RELU activation functions )
+	----------------------
+	   Min: -sqrt(2/in)
+	   Max: sqrt(2/in))
+	---------------------
+
+	For  Hyperbolic tangent activation function, tanh(x)
+	standard deviation (v) = 1 / sqrt(ave)
+	ave = (in + cu) / 2
+
+	For  Sigmoid activation function, 1 / (1 + exp(-x))
+	standard deviation (v) = 3.6 / sqrt(in)
+
+	For  RELU activation function, max(0,x)
+	standard deviation (v) = sqrt(2/in)
+
+	References
+	-------------------------------------
+	http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf
+	https://arxiv.org/pdf/1704.08863.pdf   page 3.
+	https://arxiv.org/abs/1502.01852
+	https://bit.ly/3ehISpn
+	http://ufldl.stanford.edu/tutorial/supervised/MultiLayerNeuralNetworks/
+	https://www.youtube.com/watch?v=tYFO434Lpm0
+	*/
+	int size_l = cu;
+	int size_l_minus_1 = in;
+
+	if (activefunc == "relu")
+	{
+		double min = -sqrt(2) / sqrt(size_l_minus_1);
+		double max = sqrt(2) / sqrt(size_l_minus_1);
+		for (int i = 0; i < m->rows; i++) {
+			for (int j = 0; j < m->cols; j++) {
+				m->entries[i][j] = uniform_distribution(min, max);
+			}
+		}
+	}
+	else if (activefunc == "softmax" || "tanh")
+	{
+		double min = -sqrt(2) / sqrt(size_l_minus_1 + size_l);
+		double max = sqrt(2) / sqrt(size_l_minus_1 + size_l);
+		for (int i = 0; i < m->rows; i++) {
+			for (int j = 0; j < m->cols; j++) {
+				m->entries[i][j] = uniform_distribution(min, max);
+			}
+		}
+	}
+
+
+	//double min = -(sqrt(6.0) / sqrt(size_l_minus_1 + size_l));
+	//double max = (sqrt(6.0) / sqrt(size_l_minus_1 + size_l));
+}
+
 
 void matrix_randomize(Matrix* m, int n) {
 	// Pulling from a random distribution of 
